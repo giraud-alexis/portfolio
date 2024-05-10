88c89ac5233c0708c2a2f52ad0f0c1a753c97fd5
giraud-alexis <alexis33190@gmail.com>
2024-05-10 21:40:46 +0200

    correction + ajout musique d'ambiance

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,23 @@
+const music = document.getElementById("audio-music");
+const toggleButton = document.getElementById("music-btn");
+let isPlaying = false;
 const skillsImg = document.getElementById("skills_shema");
 const projectsImg = document.getElementById("projects");
-// const project1 = document.getElementById("project_1");
-// const project2 = document.getElementById("project_2");
-// const title1 = document.querySelectorAll(".tittle_project_1");
-// const title2 = document.querySelectorAll(".tittle_project_2");
-// const des1 = document.querySelector(".des_project_1");
-// const des2 = document.querySelector(".des_project_2");
 
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 900) {
     skillsImg.style.opacity = "1";
   }
   if (window.scrollY > 2100) {
-    //projectsImg.style.marginLeft = "9%";
     projectsImg.style.opacity = "1";
-    // project1.style.opacity = "1";
-    // project2.style.opacity = "1";
-    // title1.style.opacity = "1";
-    // title2.style.opacity = "1";
-    // des1.style.opacity = "1";
-    // des2.style.opacity = "1";
   }
+});
+
+toggleButton.addEventListener("click", () => {
+  if (isPlaying) {
+    music.pause();
+  } else {
+    music.play();
+  }
+  isPlaying = !isPlaying;
 });
